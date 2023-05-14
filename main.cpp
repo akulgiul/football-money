5bc5f98ad0bdd1c0b71a93dfc53f3bce7dbb0b7f
akulgiul <luka@4luig.de>
2023-05-14 20:59:23 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author Luka Luig
+ * @brief
+ * @date 2023-05-14
+ *
+ * Copyright (c) 2023 luka luig
+ *
+ */
+
+#include <iostream>
+
+int main() {
+	// test
+	std::cout << "Hello, world!" << std::endl;
+}
